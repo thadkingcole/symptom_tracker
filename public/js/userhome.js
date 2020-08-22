208ec003299116cc930a7c8168c2a6eae977fcef
diuguide <61917141+diuguide@users.noreply.github.com>
2020-08-22 14:17:47 -0400

    grids populate, needs styling and fourth week grid

--- public/js/userhome.js
+++ public/js/userhome.js
@@ -1,34 +1,60 @@
 $(document).ready(() => {
-    const monthGrid = document.querySelector(".month-grid");
-    const dayGrid = document.querySelector(".day-grid");
-    let monthSquares = [];
-    let daySquares = [];
-
-    function createMonthGrid() {
-        for (let i = 0; i < 12; i++) {
-            let monthSquare = document.createElement("div");
-            monthSquare.classList.add("month");
-            monthSquare.setAttribute("id", i);
-            monthGrid.appendChild(monthSquare);
-            monthSquares.push(monthSquare);
-        }
+  const monthGrid = document.querySelector(".month-grid");
+  const dayGrid = document.querySelector(".day-grid");
+  const symptomGrid = document.querySelector(".symptom-grid");
+  const monthSquares = [];
+  const daySquares = [];
+  const symptomSquares = [];
+  const monthArray = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
+  function createMonthGrid() {
+    for (let i = 0; i < 12; i++) {
+      let monthSquare = document.createElement("div");
+      monthSquare.innerHTML = monthArray[i];
+      monthSquare.classList.add("month");
+      monthSquare.setAttribute("id", i);
+      monthGrid.appendChild(monthSquare);
+      monthSquares.push(monthSquare);
     }
-    function createDayGrid() {
-        for (let i = 0; i < 31; i++) {
-            let daySquare = document.createElement("div");
-            daySquare.classList.add("day");
-            daySquare.setAttribute("id", i);
-            dayGrid.appendChild(daySquare);
-            daySquares.push(daySquare);
-        }
-
+  }
+  function createDayGrid() {
+    for (let i = 0; i < 31; i++) {
+      let daySquare = document.createElement("div");
+      daySquare.classList.add("day");
+      daySquare.setAttribute("id", i);
+      dayGrid.appendChild(daySquare);
+      daySquares.push(daySquare);
     }
-    createMonthGrid();
-    createDayGrid();
-    createDayGrid();
-    createDayGrid();
-    createDayGrid();
-    createDayGrid();
-    createDayGrid();
-    createDayGrid();
+
+  }
+  function createSymptomGrid() {
+    for (let i = 0; i < 7; i++) {
+      let symptomSquare = document.createElement("div");
+      symptomSquare.classList.add("symptom");
+      symptomSquare.setAttribute("id", i);
+      symptomGrid.appendChild(symptomSquare);
+      symptomSquares.push(symptomSquare);
+    }
+  }
+  createSymptomGrid();
+  createMonthGrid();
+  createDayGrid();
+  createDayGrid();
+  createDayGrid();
+  createDayGrid();
+  createDayGrid();
+  createDayGrid();
+  createDayGrid();
 });
