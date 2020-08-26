ccb69e6d9027e84ced90b72c391433ecd916db9a
diuguide <61917141+diuguide@users.noreply.github.com>
2020-08-25 22:07:41 -0400

    travis

--- public/js/userhome.js
+++ public/js/userhome.js
@@ -71,12 +71,12 @@ $(document).ready(() => {
   function dayModal() {
     span.onclick = function () {
       modal.style.display = "none";
-    }
+    };
     window.onclick = function (event) {
       if (event.target === modal) {
         modal.style.display = "none";
       }
-    }
+    };
   }
   dayModal();
   showDates();
