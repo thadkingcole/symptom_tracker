e568481cacc775e09aacc6f80e919dc4b960fbd9
diuguide <61917141+diuguide@users.noreply.github.com>
2020-08-27 16:40:41 -0400

    more styling, mood is color coded

--- public/js/userhome.js
+++ public/js/userhome.js
@@ -2,6 +2,22 @@ document.addEventListener("DOMContentLoaded", () => {
   const modal = document.getElementById("dayModal");
   const span = document.getElementById("class");
   const checkIn = document.getElementById("checkIn");
+  const moodDiv = document.getElementById("mood");
+
+  function moodColors() {
+    console.log(moodDiv.innerHTML);
+    if (moodDiv.innerHTML == 1) {
+      moodDiv.style.backgroundColor = "red";
+    } else if (moodDiv.innerHTML == 2) {
+      moodDiv.style.backgroundColor = "orange";
+    } else if (moodDiv.innerHTML == 3) {
+      moodDiv.style.backgroundColor = "blue";
+    } else if (moodDiv.innerHTML == 4) {
+      moodDiv.style.backgroundColor = "yellow";
+    } else if (moodDiv.innerHTML == 5) {
+      moodDiv.style.backgroundColor = "green";
+    }
+  }
 
   function dailyCheckIn() {
     // add event listener to daily check-in link
@@ -22,4 +38,5 @@ document.addEventListener("DOMContentLoaded", () => {
   }
   dayModal();
   dailyCheckIn();
+  moodColors();
 });
